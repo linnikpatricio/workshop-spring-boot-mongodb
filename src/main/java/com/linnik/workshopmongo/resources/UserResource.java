c4598981942a94f2ca5d342ea6cd7f08be9caaa3
linnikpatricio <linnikpatricio17@gmail.com>
2025-02-25 18:29:38 -0300

    Atualização de usuário com PUT

--- src/main/java/com/linnik/workshopmongo/resources/UserResource.java
+++ src/main/java/com/linnik/workshopmongo/resources/UserResource.java
@@ -41,4 +41,12 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+        User obj = service.fromDTO(userDTO);
+        obj.setId(id);
+        service.update(obj);
+        return ResponseEntity.noContent().build();
+
+    }
 }
