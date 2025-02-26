c6c56cd791b3c4ef7289b2fc24fdfd370aa5ce23
linnikpatricio <linnikpatricio17@gmail.com>
2025-02-26 11:03:13 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/linnik/workshopmongo/config/Instantiation.java
+++ src/main/java/com/linnik/workshopmongo/config/Instantiation.java
@@ -1,24 +1,45 @@
 package com.linnik.workshopmongo.config;
 
+import com.linnik.workshopmongo.domain.Post;
 import com.linnik.workshopmongo.domain.User;
+import com.linnik.workshopmongo.repository.PostRepository;
 import com.linnik.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
+
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
     @Override
     public void run(String... args) throws Exception {
-        repository.deleteAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        repository.saveAll(Arrays.asList(maria,alex,bob));
+        Post post1 = new Post(null,sdf.parse("21/02/2018"),"Partiu viagem","Vou viajar para São Paulo. Abraços!",maria);
+        Post post2 = new Post(null,sdf.parse("23/02/2018"),"Bom dia","Acordei feliz hoje!",maria);
+
+        postRepository.saveAll(Arrays.asList(post1,post2));
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+
+
+
+
     }
 }
---
+++ src/main/java/com/linnik/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.linnik.workshopmongo.repository;
+
+import com.linnik.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
