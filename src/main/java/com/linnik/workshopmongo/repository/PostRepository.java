c99bcea50ab16f34c70a94e58d0b364cb087ed1d
linnikpatricio <linnikpatricio17@gmail.com>
2025-03-05 10:52:31 -0300

    Consulta simples com @Query

--- src/main/java/com/linnik/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/linnik/workshopmongo/repository/PostRepository.java
@@ -2,13 +2,15 @@ package com.linnik.workshopmongo.repository;
 
 import com.linnik.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> serachTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
